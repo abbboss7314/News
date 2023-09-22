885678842503e8f32159db51188bdc6530dff1dd
abbboss7314 <zaynidinovabbos@gmail.com>
2023-09-22 13:53:05 +0500

    Initial Commit

---
+++ 76-Lesson-HW-News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  76-Lesson-HW-News
+//
+//  Created by Death Heart on 22/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
